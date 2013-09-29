153b502e5c760e5148c5c076ecfa82849969038d
Mikhail Melnik <by.zumzoom@gmail.com>
2013-09-29 16:41:00 +0400

    Diameter minor improvements

--- include/cg/operations/diameter.h
+++ include/cg/operations/diameter.h
@@ -8,43 +8,35 @@
 
 namespace cg
 {
-	namespace util
-	{
-		template<class Scalar>
-		Scalar maxDistanceEdgePoint(const point_2t<Scalar> & p, const segment_2t<Scalar> & s)
-		{
-			return std::max(distance(s[0], p), distance(s[1], p));
-		}
-	}
-
 	template<class BidIter>
 	std::pair<BidIter, BidIter> diameter(BidIter begin, BidIter end)
 	{
-		using util::maxDistanceEdgePoint;
 		using namespace common;
 		typedef typename BidIter::value_type::value_type Scalar;
 
+		auto maxDistanceEdgePoint = [](const point_2t<Scalar> & p, const segment_2t<Scalar> & s)
+		{
+			return std::max(distance(s[0], p), distance(s[1], p));
+		};
+
 		end = andrew_hull(begin, end);
 
-		if(begin == end)
-			return std::make_pair(begin, end);
-		else if(std::next(begin) == end)
+		if(begin == end || std::next(begin) == end)
 			return std::make_pair(begin, begin);
 		else if(std::next(begin, 2) == end)
 			return std::make_pair(begin, std::next(begin));
 
-		segment_2t<Scalar> edge(*begin, *(begin + 1));
+		segment_2t<Scalar> edge(*begin, *std::next(begin));
 
 		BidIter tmp = std::max_element(begin, end,
-			[&edge](const point_2t<Scalar> & a, const point_2t<Scalar> & b) 
+			[&](const point_2t<Scalar> & a, const point_2t<Scalar> & b) 
 			{ return maxDistanceEdgePoint(a, edge) < maxDistanceEdgePoint(b, edge); });
 
 		Scalar d = 0;
 		BidIter ans1, ans2 = tmp;
-		if(make_max(d, distance(edge[0], *tmp)))
-			ans1 = begin;
-		if(make_max(d, distance(edge[1], *tmp)))
-			ans1 = begin + 1;
+		for(size_t i = 0; i < 2; ++i)
+			if(make_max(d, distance(edge[i], *tmp)))
+				ans1 = begin + i;
 
 		range_circulator<BidIter> cl(begin, end, tmp);
 
