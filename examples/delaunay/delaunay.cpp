7c6125c96b741382616652e9d8cd890c4cc09f77
Mikhail Melnik <by.zumzoom@gmail.com>
2013-11-24 00:32:29 +0400

    [+] deleting vertex

--- examples/delaunay/delaunay.cpp
+++ examples/delaunay/delaunay.cpp
@@ -5,54 +5,140 @@
 
 #include <cg/visualization/viewer_adapter.h>
 #include <cg/visualization/draw_util.h>
-
 #include <cg/triangulation/delaunay_triangulation.h>
-#include <cg/io/triangle.h>
-
 
 using cg::point_2f;
 using cg::point_2;
 using cg::triangle_2;
 
-
 struct delaunay_viewer : cg::visualization::viewer_adapter
 {
    delaunay_viewer()
-       : res(tr.get_triangulation())
+       : mode(inserting), res(tr.get_triangulation())
    {}
 
    void draw(cg::visualization::drawer_type & drawer) const
    {
       drawer.set_color(Qt::white);
-      for (point_2 p : pts)
+      for(point_2 p : pts)
          drawer.draw_point(p, 2);
 
       drawer.set_color(Qt::green);
-      for (triangle_2 t : res)
+      for(triangle_2 t : res)
       {
          drawer.draw_line(t[0], t[1]);
          drawer.draw_line(t[0], t[2]);
          drawer.draw_line(t[2], t[1]);
       }
+
+      drawer.set_color(Qt::red);
+      for(triangle_2 t : failed)
+      {
+         drawer.draw_line(t[0], t[1]);
+         drawer.draw_line(t[0], t[2]);
+         drawer.draw_line(t[2], t[1]);
+      }
+
+      drawer.set_color(Qt::yellow);
+      if(cur_pt)
+          drawer.draw_point(*cur_pt, 10);
+   }
+
+   bool on_move(const point_2f& p)
+   {
+      set_cur_pt(p);
+      return true;
+   }
+
+   bool on_key(int key)
+   {
+      switch (key)
+      {
+      case Qt::Key_I :
+         mode = inserting;
+         break;
+
+      case Qt::Key_D :
+         mode = deleting;
+         break;
+
+      default :
+         return false;
+      }
+
+      return true;
    }
 
    void print(cg::visualization::printer_type & p) const
    {
-      p.corner_stream() << "press mouse rbutton to add point" << cg::visualization::endl;
+      p.corner_stream() << "press mouse rbutton to add point" << cg::visualization::endl <<
+                           "mode: " << (mode == inserting ? "inserting" : "deleting") << cg::visualization::endl;
    }
 
    bool on_release(const point_2f & p)
    {
-      tr.add_vertex(p);
+      switch(mode)
+      {
+      case inserting:
+          tr.add_vertex(p);
+          break;
+      case deleting:
+          if(set_cur_pt(p))
+          {
+             tr.del_vertex(*cur_pt);
+             cur_pt.reset();
+          }
+          else
+             return false;
+          break;
+      default:
+          return false;
+      }
       res = tr.get_triangulation();
       pts = tr.get_points();
+      check_edges();
       return true;
    }
 
+   void check_edges()
+   {
+       failed.clear();
+       for(triangle_2 t : res)
+           for(point_2 s : pts)
+               if(t[0] != s && t[1] != s && t[2] != s && cg::contains_impl(t[0], t[1], t[2], s) > 0.0)
+                   failed.push_back(t);
+   }
+
 private:
+
+   bool set_cur_pt (const point_2f& p)
+   {
+      cur_pt.reset();
+      float max_r;
+      for (auto pt : pts)
+      {
+         float current_r = (p.x - pt.x) * (p.x - pt.x) + (p.y - pt.y) * (p.y - pt.y);
+         if ((cur_pt && current_r < max_r) || (!cur_pt && current_r < 30))
+         {
+            cur_pt = pt;
+            max_r = current_r;
+         }
+      }
+      return cur_pt;
+   }
+
+   boost::optional<point_2> cur_pt;
+
+   enum modes
+   {
+      inserting = 0,
+      deleting = 1
+   } mode;
+
    cg::delaunay_triangulation_2t<double> tr;
    std::vector<triangle_2> res;
    std::vector<point_2> pts;
+   std::vector<triangle_2> failed;
 };
 
 int main(int argc, char ** argv)
--- include/cg/triangulation/delaunay_triangulation.h
+++ include/cg/triangulation/delaunay_triangulation.h
@@ -19,12 +19,22 @@ namespace cg
     using edge_handle = std::shared_ptr < edge_2t <Scalar> >;
 
     template<class Scalar>
-    bool contains(std::vector< vertex_handle <Scalar> > t, point_2t <Scalar> const & q)
+    bool contains(const std::vector< vertex_handle <Scalar> > & t, point_2t <Scalar> const & q)
     {
        for (size_t l = 0, lp = 2; l != 3; lp = l++)
            if (!t[lp]->inf() && !t[l]->inf() && orientation(*t[lp], *t[l], q) == CG_RIGHT)
               return false;
        return true;
+    }
+
+    template<class Scalar>
+    Scalar contains_impl(const point_2t<Scalar> & a, const point_2t<Scalar> & b,
+                         const point_2t<Scalar> & c, const point_2t<Scalar> & d)
+    {
+        Scalar a11 = a.x - d.x, a12 = a.y - d.y, a13 = (a.x * a.x - d.x * d.x) + (a.y * a.y - d.y * d.y);
+        Scalar a21 = b.x - d.x, a22 = b.y - d.y, a23 = (b.x * b.x - d.x * d.x) + (b.y * b.y - d.y * d.y);
+        Scalar a31 = c.x - d.x, a32 = c.y - d.y, a33 = (c.x * c.x - d.x * d.x) + (c.y * c.y - d.y * d.y);
+        return a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31);
     }
 
     template<class Scalar>
@@ -40,10 +50,12 @@ namespace cg
        else if(d->inf())
            return false;
 
-       double a11 = a->x - d->x, a12 = a->y - d->y, a13 = (a->x * a->x - d->x * d->x) + (a->y * a->y - d->y * d->y);
-       double a21 = b->x - d->x, a22 = b->y - d->y, a23 = (b->x * b->x - d->x * d->x) + (b->y * b->y - d->y * d->y);
-       double a31 = c->x - d->x, a32 = c->y - d->y, a33 = (c->x * c->x - d->x * d->x) + (c->y * c->y - d->y * d->y);
-       return a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31) > 0;
+//       double a11 = a->x - d->x, a12 = a->y - d->y, a13 = (a->x * a->x - d->x * d->x) + (a->y * a->y - d->y * d->y);
+//       double a21 = b->x - d->x, a22 = b->y - d->y, a23 = (b->x * b->x - d->x * d->x) + (b->y * b->y - d->y * d->y);
+//       double a31 = c->x - d->x, a32 = c->y - d->y, a33 = (c->x * c->x - d->x * d->x) + (c->y * c->y - d->y * d->y);
+//       return a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31) > 0;
+
+       return contains_impl(*a, *b, *c, *d) > 0;
     }
 
     template <class Scalar>
@@ -60,9 +72,22 @@ namespace cg
             _e = e;
         }
 
-        edge_handle<Scalar> edge() const
+        void clear_edge()
         {
-            return _e;
+            _e.reset();
+        }
+
+        face_handle<Scalar> face(size_t ind = 0) const
+        {
+            return edge(ind)->face();
+        }
+
+        edge_handle<Scalar> edge(size_t ind = 0) const
+        {
+            edge_handle<Scalar> edge = _e;
+            for(size_t i = 0; i < ind; ++i)
+                edge = edge->neighbour();
+            return edge;
         }
 
         bool inf() const
@@ -82,12 +107,12 @@ namespace cg
 
         face_2t(const edge_handle<Scalar> & e) : _e(e) {}
 
-        vertex_handle<Scalar> vertex(size_t index) const
+        vertex_handle<Scalar> vertex(size_t index = 0) const
         {
             return edge(index)->vertex();
         }
 
-        edge_handle<Scalar> edge(size_t index) const
+        edge_handle<Scalar> edge(size_t index = 0) const
         {
             edge_handle<Scalar> e = _e;
             for(size_t i = 0; i < index; ++i)
@@ -103,6 +128,11 @@ namespace cg
         void set_edge(const edge_handle<Scalar> & e)
         {
             _e = e;
+        }
+
+        void clear_edge()
+        {
+            _e.reset();
         }
 
     private:
@@ -137,6 +167,11 @@ namespace cg
             return _next->next();
         }
 
+        edge_handle<Scalar> neighbour() const
+        {
+            return _twin->next();
+        }
+
         void set_twin(const edge_handle<Scalar> & twin)
         {
             _twin = twin;
@@ -157,11 +192,61 @@ namespace cg
             _v = v;
         }
 
+        void clear_twin()
+        {
+            _twin.reset();
+        }
+
+        void clear_next()
+        {
+            _next.reset();
+        }
+
+        void clear_face()
+        {
+            _f.reset();
+        }
+
+        void clear_vertex()
+        {
+            _v.reset();
+        }
+
     private:
         vertex_handle<Scalar> _v;
         face_handle<Scalar> _f;
         edge_handle<Scalar> _twin, _next;
     };
+
+    template <class Scalar>
+    std::ostream & operator<<(std::ostream & os, const face_handle<Scalar> & face)
+    {
+        os << "(" << face->vertex(0) << ", " << face->vertex(1) << ", " << face->vertex(2) << ")\n" <<
+              "edge0: " << face->edge(0) << ", twin " << face->edge(0)->twin() <<
+              ", face vertex " << face->edge(0)->face()->vertex() << ", next " <<
+              face->edge(0)->next() << "\n"<<
+              "edge1: " << face->edge(1) << ", twin " << face->edge(1)->twin() <<
+              ", face vertex " << face->edge(1)->face()->vertex() << ", next " <<
+              face->edge(1)->next() << "\n"<<
+              "edge2: " << face->edge(2) << ", twin " << face->edge(2)->twin() <<
+              ", face vertex " << face->edge(2)->face()->vertex() << ", next " <<
+              face->edge(2)->next() << "\n";
+        return os;
+    }
+
+    template <class Scalar>
+    std::ostream & operator<<(std::ostream & os, const edge_handle<Scalar> & edge)
+    {
+        os << "(" << *edge->vertex() << ", " << *edge->next()->vertex() << ")";
+        return os;
+    }
+
+    template <class Scalar>
+    std::ostream & operator<<(std::ostream & os, const vertex_handle<Scalar> & vertex)
+    {
+        os << "(" << *vertex << ")";
+        return os;
+    }
 
     template <class Scalar>
     struct delaunay_triangulation_2t
@@ -236,8 +321,91 @@ namespace cg
                     for(size_t i = 0; i < 3; ++i)
                     {
                         fix_correctness(edge->next());
-                        edge = edge->twin()->next();
+                        edge = edge->neighbour();
                     }
+                    return;
+                }
+            }
+        }
+
+        void del_vertex(const point_2t<Scalar> & p)
+        {
+            if(_vtxs.size() <= 3)
+            {
+                for(typename std::vector < vertex_handle <Scalar> >::const_iterator it = _vtxs.begin(); it != _vtxs.end();)
+                    if(**it == p)
+                        it = _vtxs.erase(it);
+                    else
+                        ++it;
+                _edgs.clear();
+                _fcs.clear();
+                return;
+            }
+
+            for(typename std::vector < vertex_handle <Scalar> >::const_iterator it = _vtxs.begin(); it != _vtxs.end(); ++it)
+            {
+                auto vtx = *it;
+                if(*vtx == p)
+                {
+                    std::vector< edge_handle <Scalar> > flipped;
+                    edge_handle<Scalar> edge = vtx->edge();
+                    size_t count = 1;
+                    while(edge->neighbour() != vtx->edge())
+                    {
+                        ++count;
+                        edge = edge->neighbour();
+                    }
+
+                    for(size_t i = 3; i < count; ++i)
+                    {
+                        edge_handle<Scalar> to_flip = edge;
+                        edge = edge->neighbour();
+                        flip(to_flip);
+                        flipped.push_back(to_flip);
+                    }
+
+                    _vtxs.erase(it);
+
+                    face_handle<Scalar> face = vtx->face();
+                    edge = vtx->edge()->next();
+                    for(size_t i = 0; i < 3; ++i)
+                    {
+                        vtx->edge(i)->next()->set_next(vtx->edge(i)->prev()->neighbour());
+                        vtx->edge(i)->next()->vertex()->set_edge(vtx->edge(i)->next());
+                        vtx->edge(i)->next()->face()->clear_edge();
+                        if(vtx->edge(i)->face() != face)
+                        {
+                            auto iter = std::find(_fcs.begin(), _fcs.end(), vtx->edge(i)->face());
+                            _fcs.erase(iter);
+                        }
+                        vtx->edge(i)->next()->set_face(face);
+                    }
+
+                    face->set_edge(edge);
+
+                    edge_handle<Scalar> edgs[6];
+                    for(size_t i = 0; i < 3; ++i)
+                    {
+                        edgs[2 * i] = vtx->edge(i);
+                        edgs[2 * i + 1] = vtx->edge(i)->twin();
+                    }
+
+                    for(size_t i = 0; i < 6; ++i)
+                    {
+                        edgs[i]->clear_face();
+                        edgs[i]->clear_next();
+                        edgs[i]->clear_twin();
+                        edgs[i]->clear_vertex();
+                        auto iter = std::find(_edgs.begin(), _edgs.end(), edgs[i]);
+                        _edgs.erase(iter);
+                    }
+
+                    for(auto edge : flipped)
+                        flip(edge);
+
+                    for(auto edge : flipped)
+                        fix_correctness(edge);
+
                     return;
                 }
             }
@@ -288,6 +456,8 @@ namespace cg
             {
                 flip(e0);
                 fix_correctness(e0->next());
+                fix_correctness(e0->prev());
+                fix_correctness(e0->twin()->next());
                 fix_correctness(e0->twin()->prev());
             }
         }
@@ -303,6 +473,8 @@ namespace cg
             twin_face->set_edge(cur_twin);
             twin_prev->set_next(cur_edge->next());
             prev_edge->set_next(cur_twin->next());
+            cur_edge->vertex()->set_edge(cur_twin->next());
+            cur_twin->vertex()->set_edge(cur_edge->next());
             cur_twin->set_vertex(twin_prev->vertex());
             cur_edge->set_vertex(prev_edge->vertex());
             cur_edge->next()->set_next(cur_edge);
